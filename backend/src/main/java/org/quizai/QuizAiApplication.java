d2cc9b96881fb6830de28665005f65f80f7a27f9
ALBON IDRIZI <albonidrizi@gmail.com>
2024-12-04 21:46:43 +0100

    Initial commit for Spring Boot Quiz API project - backend setup

---
+++ backend/src/main/java/org/quizai/QuizAiApplication.java
@@ -0,0 +1,13 @@
+package org.quizai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizAiApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/quizai/QuizAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.quizai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
